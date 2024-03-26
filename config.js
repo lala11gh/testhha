877946de3bb73443c55b873421e4e40465ac33af
lala11gh <164965340+lala11gh@users.noreply.github.com>
2024-03-26 00:48:52 +0000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    discord:{
+        id: "527108773123325980", 
+    },
+    github:{
+        token1: "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"
+    },
+    about:{
+        description: "I'm a 17 years old developer from Morocco. I'm currently working on my own projects and learning new things.",
+    },
+    social:{
+        github: "https://github.com/y0nyy",
+        instagram: ""
+    },
+    meta:{
+        title: 'My Personal Website',
+        description: 'My Personal Website',
+        theme: '#02060f',
+        lang: 'en',
+        url: 'https://imyony.com',
+        image: '',
+    },
+    title: 'Yony',
+    titles:[
+        {title: 'Home', url: '/', icon: 'fa-house'},
+        {title: 'Projects', url: '/projects' ,icon: 'fa-blog'},
+        {title: 'Blog', url: '/blog' , icon: 'fa-code'},
+    ]
+}
+
